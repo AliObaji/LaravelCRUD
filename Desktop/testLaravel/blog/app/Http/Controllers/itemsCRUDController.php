8d3a663a1b8c116c211329cb1523052b78fe31d9
AliObaji <ali-obaji@hotmail.com>
2017-04-05 14:34:05 +0300

    final touches

--- Desktop/testLaravel/blog/app/Http/Controllers/itemsCRUDController.php
+++ Desktop/testLaravel/blog/app/Http/Controllers/itemsCRUDController.php
@@ -8,14 +8,23 @@ use laravelCRUD\Item;
 class itemsCRUDController extends Controller
 {
 
+    //this function returns the main view of the blog
     public function index()
     {
         $items = Item::orderBy('id','DESC')->paginate(5);
         return view('ItemCRUD.index',compact('items'));
     }
+
+    //this function takes us to the 'create new item' form
     public function create(){
         return view('ItemCRUD.create');
     }
+
+    /*
+     * @params: item title and description
+     * adds the item to the corresponding table
+     * @returns: main view with success message
+     */
     public function store(Request $request){
         $this->validate($request,[
             "title" => 'required',
@@ -28,22 +37,42 @@ class itemsCRUDController extends Controller
 
     }
 
+    /*
+     * @params: selected item id
+     * @returns: item details view
+     */
     public function show($id){
         $itemRequested = Item::find($id);
         return view('ItemCRUD.show',compact('itemRequested'));
 
     }
+
+    /*
+     * @params: item id
+     * deletes the item from the corresponding table
+     * @returns: main view with success message
+     */
     public function destroy($id){
         Item::find($id)->delete();
         return redirect()-> route('itemsCRUD.index')
             ->with('success','Item deleted successfully');
 
     }
+
+    /*
+     * @params: item id
+     * @returns: item editor view, with the item details as parameter.
+     */
     public function edit($id){
         $item = Item::find($id);
         return view('ItemCRUD.edit',compact('item'));
     }
 
+    /*
+     * @params: item title,description, and id
+     * edits the item in the corresponding table
+     * @returns: main view with success message
+     */
     public function update(Request $request,$id){
         $this->validate($request,[
             'title' => 'required',
